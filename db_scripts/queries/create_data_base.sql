2511a534a026023909bc4939eaeaebadc8765f6a
jcmtya <70582957+jcmtya@users.noreply.github.com>
2026-04-17 06:39:00 -0500

    Delete db_scripts directory

--- db_scripts/queries/create_data_base.sql
+++
@@ -1,6 +0,0 @@
-SELECT 'CREATE DATABASE bookstore'
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM pg_database
-    WHERE datname = 'bookstore'
-)\gexec 
--- db_scripts/queries/verify.sql
+++
@@ -1,5 +0,0 @@
-\dt
-
-SELECT title, author, price FROM books LIMIT 5;
-SELECT first_name, last_name, email FROM customers;
-SELECT order_id, order_status, total_amount FROM orders;
